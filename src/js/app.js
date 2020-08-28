e5e57f79b54da6bc82ac21eb9e03e5bc88755c02
dusty <dusty@yandex-team.ru>
2020-08-28 18:39:02 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
 // TODO: write code here
 
-// comment this to pass build
-const unusedVariable = 'variable';
-
 // for demonstration purpose only
 export default function demo(value) {
   return value;
